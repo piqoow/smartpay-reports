a7f4fbe62eaef0d1a5a99435d374a58b0b5b5f88
rofiqr <piqsworkmail@gmail.com>
2025-02-01 12:54:28 +0700

    update

---
+++ application/views/iot/index.php
@@ -0,0 +1,63 @@
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Add New IoT Form</h1>
+    <form action="<?= base_url('iot/submit'); ?>" method="post">
+        <div class="form-group">
+            <label for="location_name">Location Name</label>
+            <input type="text" class="form-control" id="location_name" name="location_name" required>
+        </div>
+        <div class="form-group">
+            <label for="server_category">Server Category</label>
+            <select class="form-control" name="server_category" id="server_category" required>
+                <option value="Linux Server">Linux Server</option>
+                <option value="Windows Server">Windows Server</option>
+                <option value="IOT System">IOT System</option>
+                <option value="Infra">Infrastructure</option>
+                <option value="IT Support">IT Support</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="server_category">Reporter Email</label>
+            <input type="email" class="form-control" id="email" name="reporter_email" placeholder="Leave blank if there is no email">
+        </div>
+        <div class="form-group">
+            <label for="phone">Phone</label>
+            <input type="text" class="form-control" id="phone" name="reporter_phone" required>
+        </div>
+        <div class="form-group">
+            <label for="location">Location Name</label>
+            <input type="text" class="form-control" id="location" name="location" required>
+        </div>
+        <div class="form-group">
+            <label for="issue_date">Issue Date</label>
+            <input type="date" class="form-control" id="issue_date" name="issue_date" required>
+        </div>
+        <div class="form-group">
+            <label for="category">Division</label>
+            <select class="form-control" name="category" id="category" required>
+                <option value="Network">Network</option>
+                <option value="Parkee System">Parkee System</option>
+                <option value="IOT System">IOT System</option>
+                <option value="Infra">Infrastructure</option>
+                <option value="IT Support">IT Support</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="priority">Priority</label>
+            <select class="form-control" name="priority" id="priority" required>
+                <option value="High">High - 2 Days</option>
+                <option value="Medium">Medium - 4 Days</option>
+                <option value="Low">Low 6 - Days</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="issue_title">Issue Title</label>
+            <input class="form-control" id="issue_title" name="issue_title" required></input>
+        </div>
+        <div class="form-group">
+            <label for="issue_description">Issue Description</label>
+            <textarea class="form-control" id="issue_description" name="issue_description" rows="4" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
