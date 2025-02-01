ca893f08056441b4f6d4a29911dade6e28b1b0a4
rofiqr <piqsworkmail@gmail.com>
2025-02-01 15:34:11 +0700

    update

--- application/views/auth/v_login.php
+++ application/views/auth/v_login.php
@@ -1,24 +1,28 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="<?php echo base_url('assets/css/login.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/login.css'); ?>">
 <div class="main">
     <div class="container">
         <center>
-            <div class="middle">
+            <div class="card">
                 <div id="login">
-                    <form action="<?php echo site_url('auth/login'); ?>" method="post">
-                        <fieldset class="clearfix">
-                            <p><span class="fa fa-user"></span><input type="text" name="username" placeholder="Username" required></p>
-                            <p><span class="fa fa-lock"></span><input type="password" name="password" placeholder="Password" required></p>
-                            <div>
-                                <!-- <span style="width:48%; text-align:left; display: inline-block;"><a class="small-text" href="#">Forgot password?</a></span> -->
-                                <span style="width:48%; text-align:left; display: inline-block;"><a class="small-text" href="#"></a></span>
-                                <span style="width:50%; text-align:right; display: inline-block;"><input type="submit" value="Sign In"></span>
+                    <form action="<?= site_url('auth/login'); ?>" method="post">
+                        <!-- <fieldset class="clearfix"> -->
+                            <h3 class="card-title"></h3>
+                            <p><input type="text" name="username" placeholder="Username" required></p>
+                            <p><input type="password" name="password" placeholder="Password" required></p>
+                            <div class="form-footer">
+                                <span style="width:50%; text-align:right; display: inline-block;">
+                                    <input type="submit" value="Sign In">
+                                    <!-- <input type="submit" value="Reset Password"> -->
+                                </span>
                             </div>
-                        </fieldset>
+                        <!-- </fieldset> -->
                     </form>
-                    <div class="clearfix"></div>
                 </div>
-                <div class="logo"><img style="width: 200px;" src="<?php echo base_url('assets/images/logo_cp.png'); ?>" alt="Logo"><div class="clearfix"></div></div>
+                <div class="logo">
+                    <img src="<?= base_url('assets/images/logo_cp.png'); ?>" alt="Logo">
+                    <p>Powered by SmartPay Technology</p>
+                </div>
             </div>
         </center>
     </div>
@@ -44,7 +48,6 @@
             "width": "auto",
             "white-space": "nowrap" 
         };
-    
 
         $(document).ready(function() {
             <?php if ($this->session->flashdata('success')): ?>
