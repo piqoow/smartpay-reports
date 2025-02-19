b8db4e241fbb7dc5d3c5c540801682cb64dcba6e
rofiqr <piqsworkmail@gmail.com>
2025-02-19 12:35:20 +0700

    big update

--- application/controllers/Administration.php
+++ application/controllers/Administration.php
@@ -6,6 +6,7 @@ class Administration extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->helper(['url', 'form']);
+        $this->load->library('form_validation');
         $this->load->library('session');
         $this->load->model('M_administration');
     }
