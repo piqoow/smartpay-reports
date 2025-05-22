fe13f2494eb1f8839dad72dad4d3617a4edf17f9
rofiqr <piqsworkmail@gmail.com>
2025-05-22 20:08:31 +0700

    update

--- application/controllers/IOT.php
+++ application/controllers/IOT.php
@@ -26,8 +26,9 @@ class IOT extends CI_Controller {
         $this->load->view('templates/v_header');
     }
 
-    public function add() {
+    public function addServer() {
         date_default_timezone_set('Asia/Jakarta');
+        $datenow = new \DateTime();
         
         $data = array(
             'location_name' => $this->input->post('location_name'),
@@ -42,13 +43,15 @@ class IOT extends CI_Controller {
             'ssh_password' => $this->input->post('ssh_password'),
             'anydesk_id' => $this->input->post('anydesk_id'),
             'anydesk_password' => $this->input->post('anydesk_password'),            
-            'implementation_date' => $this->input->post('implementation_date'),            
+            'implementation_date' => $this->input->post('implementation_date'),      
+            'created_at' => $datenow->format('Y-m-d H:i:s'),
+            'updated_at' => $datenow->format('Y-m-d H:i:s'),      
         );
 
         if ($this->M_iot->saveData($data)) {
-            $this->session->set_flashdata('success', 'The complaint was successfully sent');
+            $this->session->set_flashdata('success', 'Server IoT Added Successfully');
         } else {
-            $this->session->set_flashdata('error', 'Complaint failed to send');
+            $this->session->set_flashdata('error', 'Server IoT Failed to Add');
         }
         redirect('iot-server');
     }
@@ -72,4 +75,66 @@ class IOT extends CI_Controller {
     //======================================= TDS =======================================// 
     
     //======================================= EB =======================================// 
+    
+    //======================================= EV =======================================// 
+
+    public function indexEV() {
+        if (!$this->session->userdata('logged_in')) {
+            redirect('login');
+        }
+        $data['dashboard'] = $this->M_iot->get_dashboard_data_ev(); 
+        $data['locations'] = $this->M_iot->get_locations(); 
+        $data['title'] = 'IOT Form';
+        $data['content'] = 'iot/index-ev';
+        $this->load->view('templates/main', $data);
+        $this->load->view('templates/v_footer');
+        $this->load->view('templates/v_header');
+    }
+
+    public function addEV() {
+        date_default_timezone_set('Asia/Jakarta');
+        $datenow = new \DateTime();
+        
+        $data = array(
+            'location_name' => $this->input->post('location_name'),
+            'operation_system' => $this->input->post('os_server'),
+            'ip_address' => $this->input->post('ip_address'),
+            'port' => $this->input->post('port'),
+            'category' => $this->input->post('iot_category'),
+            'database_username' => $this->input->post('database_username'),
+            'database_password' => $this->input->post('database_password'),
+            'database_name' => $this->input->post('database_name'),
+            'ssh_username' => $this->input->post('ssh_username'),
+            'ssh_password' => $this->input->post('ssh_password'),
+            'anydesk_id' => $this->input->post('anydesk_id'),
+            'anydesk_password' => $this->input->post('anydesk_password'),            
+            'implementation_date' => $this->input->post('implementation_date'),      
+            'created_at' => $datenow->format('Y-m-d H:i:s'),
+            'updated_at' => $datenow->format('Y-m-d H:i:s'),      
+        );
+
+        if ($this->M_iot->saveData_ev($data)) {
+            $this->session->set_flashdata('success', 'Server IoT Added Successfully');
+        } else {
+            $this->session->set_flashdata('error', 'Server IoT Failed to Add');
+        }
+        redirect('iot-ev');
+    }
+
+    public function deleteEV() {
+        $id = $this->input->post('id');
+        
+        if ($id) {
+            if ($this->M_iot->deleteData($id)) {
+                $this->session->set_flashdata('success', 'IOT data has been successfully deleted');
+            } else {
+                $this->session->set_flashdata('error', 'Failed to delete IOT data');
+            }
+        }
+        
+        // Redirect kembali ke halaman IOT
+        redirect('iot-ev');
+    }
+
+
 }
--- application/models/M_iot.php
+++ application/models/M_iot.php
@@ -38,4 +38,39 @@ class M_iot extends CI_Model {
     }
 
     //======================================= PGS =======================================// 
+    
+    //======================================= PGS =======================================// 
+    
+    //======================================= PGS =======================================// 
+    
+    //======================================= EV =======================================// 
+
+    public function get_dashboard_data_ev() {
+        $this->db->select('*');
+        $this->db->from('ev_system');
+        $this->db->order_by('created_at', 'ASC'); 
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function get_locations_ev() {
+        $this->locations->order_by('nama_Lokasi', 'ASC');
+        $query = $this->locations->get('ms_lokasi');
+        return $query->result();
+    }
+
+    public function saveData_ev($data) {
+        $this->db->insert('ev_system', $data);
+        return $this->db->insert_id();
+    }
+
+    public function get_all_issues_ev() {
+        $query = $this->db->get('ev_system');
+        return $query->result();
+    }
+
+    public function deleteData_ev($id) {
+        $this->db->where('id_iot', $id);
+        return $this->db->delete('ev_system');
+    }
 }
